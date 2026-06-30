d4f0a09794b416fcaebddf7e5613707f7d2cbeb7
Manthan Mehar <manthanmehar1@gmail.com>
2026-06-30 23:56:59 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (25.93%) - LeetSync

--- easy-ones/28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.py
+++ easy-ones/28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.py
@@ -3,7 +3,7 @@ class Solution:
         if len(needle) > len(haystack):
             return -1
     
-        for i in range(len(haystack)):
+        for i in range(len(haystack) - (len(needle) -1) ):
             if haystack[i:i+ len(needle)] == needle:
                 return i
         return -1
